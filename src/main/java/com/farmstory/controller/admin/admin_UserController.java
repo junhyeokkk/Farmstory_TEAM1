606844d7c553b1a8007312d7ebc00ec6fe0716e0
DESKTOP-43CC23G\lotte4 <loveu9911@naver.com>
2024-10-01 17:42:33 +0900

    admin update

--- src/main/java/com/farmstory/controller/admin/admin_UserController.java
+++ src/main/java/com/farmstory/controller/admin/admin_UserController.java
@@ -1,6 +1,7 @@
 package com.farmstory.controller.admin;
 
 import com.farmstory.dto.UserDTO;
+import com.farmstory.entity.User;
 import com.farmstory.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -29,6 +31,49 @@ public class admin_UserController {
 
         if (user != null) {
             return ResponseEntity.ok(user); // 200 OK와 함께 사용자 정보 반환
+        } else {
+            return ResponseEntity.notFound().build(); // 404 Not Found
+        }
+    }
+
+    @PostMapping("/admin/modifyRole")
+    public ResponseEntity<UserDTO> modifyRole(@RequestBody Map<String, String> payload) {
+        String uid = payload.get("uid");
+
+        UserDTO user = userService.selectUser(uid);
+
+        if(payload.get("role").equals("LEAVE")) {
+            // 탈퇴할 회원일시 id, regdate, leavedate, role 빼고 생성자로 null 처리
+            user = new UserDTO(uid, user.getRegDate(), user.getLeaveDate(), user.getRole());
+            user.setLeaveDate(String.valueOf(LocalDateTime.now()));
+        }
+
+        user.setRole(payload.get("role"));
+
+        UserDTO resultUser = userService.modifyUser(user);
+
+        if (resultUser != null) {
+            return ResponseEntity.ok(resultUser); // 200 OK와 함께 사용자 정보 반환
+        } else {
+            return ResponseEntity.notFound().build(); // 404 Not Found
+        }
+    }
+
+    @PostMapping("/admin/quitUser")
+    public ResponseEntity<UserDTO> quitUser(@RequestBody Map<String, String> payload) {
+        String uid = payload.get("uid");
+
+        UserDTO user = userService.selectUser(uid);
+        UserDTO quituser = new UserDTO();
+
+        quituser.setUid(uid);
+        quituser.setRegDate(user.getRegDate());
+        quituser.setLeaveDate(String.valueOf(LocalDateTime.now()));
+
+        UserDTO resultUser = userService.modifyUser(quituser);
+
+        if (resultUser != null) {
+            return ResponseEntity.ok(resultUser); // 200 OK와 함께 사용자 정보 반환
         } else {
             return ResponseEntity.notFound().build(); // 404 Not Found
         }
--- src/main/java/com/farmstory/dto/UserDTO.java
+++ src/main/java/com/farmstory/dto/UserDTO.java
@@ -28,5 +28,22 @@ public class UserDTO {
     private String leaveDate;
 
 
+    // 탈퇴 회원을 위한 생성자
+    public UserDTO(String uid, String regDate, String leaveDate, String role) {
+        this.uid = uid;
+        this.regDate = regDate;
+        this.leaveDate = leaveDate;
+        this.role = role;
+        this.name = null;
+        this.email = null;
+        this.pass = null;
+        this.nick = null;
+        this.hp = null;
+        this.point = 0;
+        this.zip = null;
+        this.addr1 = null;
+        this.addr2 = null;
+        this.regip = null;
+    }
 
 }
