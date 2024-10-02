ff59fc14c027b296d8bf0ce58da1da7c8669c237
hasoro <noily4748@gmail.com>
2024-10-02 15:12:50 +0900

    KJW_10-02 15:10

--- src/main/java/com/farmstory/repository/CartRepository.java
+++ src/main/java/com/farmstory/repository/CartRepository.java
@@ -5,7 +5,9 @@ import com.farmstory.repository.custom.CartRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Integer>, CartRepositoryCustom {
-
+    public List<Cart> findAllByUid(String uid);
 }
