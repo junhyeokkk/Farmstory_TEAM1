30cf8995053f28d55247c10f494f7d0f1a1ce9a2
jin123346 <hajhi7899@gmail.com>
2024-10-04 13:13:48 +0900

    update article

--- src/main/java/com/farmstory/controller/CustomErrorController.java
+++ src/main/java/com/farmstory/controller/CustomErrorController.java
@@ -3,17 +3,22 @@ package com.farmstory.controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Log4j2
 @Controller
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Throwable exception = (Throwable) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        if (exception != null) {
+            log.error("Error occurred", exception);
+        }
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
             model.addAttribute("status", statusCode);
